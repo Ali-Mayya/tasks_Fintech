a988405b9eb5591cd084d191ede668b89934eb09
Ali-Mayya <93834147+Ali-Mayya@users.noreply.github.com>
2023-07-27 18:03:59 +0300

    Update inetgration_googleAPI.py

--- inetgration_googleAPI.py
+++ inetgration_googleAPI.py
@@ -1,34 +1,44 @@
 import requests
 
 def get_geocode(address, api_key):
-    # URL для запроса к Google Maps API
+    # URL for the Google Maps API request
     url = f'https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={api_key}'
 
-    # Отправляем GET-запрос к API
+    # Send a GET request to the API
     response = requests.get(url)
 
-    # Проверяем успешность запроса
+    # Check if the request was successful
     if response.status_code == 200:
         data = response.json()
 
-        # Проверяем, что API вернул результаты геокодирования
+        # Check if the API returned geocoding results
         if data['status'] == 'OK':
-            # Получаем координаты из ответа API
+            # Extract coordinates from the API response
             location = data['results'][0]['geometry']['location']
             latitude = location['lat']
             longitude = location['lng']
 
-            print(f'Широта: {latitude}, Долгота: {longitude}')
+            print(f'Latitude: {latitude}, Longitude: {longitude}')
         else:
-            print('Ошибка геокодирования:', data['status'])
+            print('Geocoding error:', data['status'])
     else:
-        print('Ошибка при запросе к API:', response.status_code)
+        print('Error in API request:', response.status_code)
 
 if __name__ == "__main__":
-    # Замените 'YOUR_API_KEY' на свой ключ API Google Maps
+    # Replace 'YOUR_API_KEY' with your own Google Maps API key
     api_key = 'AIzaSyBrarqLgaofy3crNi9GHSIccI4QdZizHIE'
 
-    address = '1600 Amphitheatre Parkway, Mountain View, CA'
+    address = 'Red Square, Moscow, Russia'
 
-
+    # Call the function to get the geocode for the given address
     get_geocode(address, api_key)
+
+
+
+
+
+
+
+
+
+
